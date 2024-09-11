9539bb7bbe597cfbc3e2aec4d5a657f1b75c28c8
Rangdiff <bengkelnodi222@gmail.com>
2024-09-11 13:58:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:kas_personal/providers/kas_provider.dart';
+import 'package:kas_personal/router.dart';
+import 'package:kas_personal/theme.dart';
+import 'package:provider/provider.dart';
+
+void main () {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => KasProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      theme: theme,
+      routerConfig: router,
+   );
+  }
+}
